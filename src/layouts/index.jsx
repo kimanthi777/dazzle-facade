eb1df43f919757c6077f009fc652b039a87e96cf
Qodestackr <winchygichu@gmail.com>
2022-06-22 00:30:45 +0300

    basic scaffold

---
+++ src/layouts/index.jsx
@@ -0,0 +1,20 @@
+
+import HeaderNav from './HeaderNav'
+import HeroSection from './HeroSection'
+import Pricing from './Pricing'
+import FreeTrial from './FreeTrial'
+import Footer from './Footer'
+import Features from './Features'
+
+const Index = () => (
+    <>
+    <HeaderNav />
+    <HeroSection />
+    <Features/>
+    <Pricing />
+    <FreeTrial />
+    <Footer />
+    </>
+)
+
+export default Index
