96e87ecd0b4e9de3dbdacfc5d3bd7aef1b3a2596
Qodestackr <winchygichu@gmail.com>
2022-12-05 15:54:42 +0300

    ...progress :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
 import React from 'react'
+import './App.css'
 
-//@clerk setup
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  // UserButton,
-  useUser,
-  RedirectToSignIn,
-} from '@clerk/clerk-react';
-// #hooks
-import { useNavigate } from "react-router-dom";
+import { HeaderNav } from './layouts';
+import { Footer } from './layouts';
+import { Home } from './pages/home';
 
-import './App.css';
-import Index from './layouts';
-import { Text } from './core/components'
-
-// @clerk key
-const FRONTEND_KEY = process.env.REACT_APP_CLERK_FRONTEND_API
 
 function App() {
-
-  const navigate = useNavigate();
-
   return (
     <>
-      {/* <ClerkProvider frontendApi={FRONTEND_KEY} navigate={(to) => navigate(to)}> */}
-      {/* <SignedIn />
-      <SignedOut>
-      <RedirectToSignIn />
-      </SignedOut> */}
-
-      <Index />
-      <Text as='label' htmlFor='header' className='text-2xl text-indigo-500 flex justify-center items-center'>Hello</Text>
-
-      {/* </ClerkProvider> */}
+      <HeaderNav />
+      <Home />
+      <Footer />
     </>
   );
 }
