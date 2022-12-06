ee749f2ce50711c52347210b857f9f0268fc25cb
Qodestackr <winchygichu@gmail.com>
2022-12-06 17:44:19 +0300

    ..

--- src/pages/home/hero-section/HeroSection.tsx
+++ src/pages/home/hero-section/HeroSection.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
 // components
 import { Text } from '../../../core/components/Common'
+
 // assets
 import playSVG from "../../../assets/images/play.svg"
 import Banner3 from "../../../assets/images/banner3-1.png"
@@ -25,13 +27,14 @@ const HeroSection = () => {
 
                     <ul className="flex space-x-4">
                         <li>
-                            <a href="/" className="px-3 py-2 bg-white text-black rounded-md hover:bg-[#FE6B7E] hover:text-white">
+                            <Link to="/" className="px-3 py-2 bg-white text-black rounded-md hover:bg-[#FE6B7E] hover:text-white">
                                 Request Demo
-                            </a></li>
+                            </Link>
+                            </li>
                         <li>
-                            <a href="/" className="flex items-center"><img className="w-5 h-5" src={playSVG} alt="" /> <span
+                            <Link to="/" className="flex items-center"><img className="w-5 h-5" src={playSVG} alt="" /> <span
                                 className="ml-1">How it works</span>
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
