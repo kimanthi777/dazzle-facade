49a8ce1d37141876f07ef539d166d2112173af7b
Qodestackr <winchygichu@gmail.com>
2022-12-06 12:59:04 +0300

    minor edits: QR, absolute imports couldn't work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import './App.css'
 
-import { HeaderNav } from './layouts';
-import { Footer } from './layouts';
+import { HeaderNav, Footer } from './layouts';
 import { Home } from './pages/home';
-
 
 function App() {
   return (
