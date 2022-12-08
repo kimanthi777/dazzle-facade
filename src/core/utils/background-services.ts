09da1d8ec69dcc5a15f1aa52d5655036c0a7df33
Qodestackr <winchygichu@gmail.com>
2022-12-08 10:11:12 +0300

    background services & cache

---
+++ src/core/utils/background-services.ts
@@ -0,0 +1,7 @@
+/***
+ * ? Utility functions to handle service workers,
+ * ?include: data fetching{sockets}, background sync, payments API, push notifications, Reporting API, and more
+ * 
+ */
+
+export { }
