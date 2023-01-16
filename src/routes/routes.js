c90c3cdfa9100d967009208f02c41b87dba6f35d
Qodestackr <winchygichu@gmail.com>
2023-01-16 13:45:01 +0300

    --changes

---
+++ src/routes/routes.js
@@ -0,0 +1,24 @@
+
+const DashboardRoutes = [
+    'Personal Info', 'Salary', 'Documents',
+    'Attendance', 'Absence', 'Performance',
+    'Onboarding', 'History', 'Roles', 'Notes',
+
+]
+
+const extraRoutes = [
+    'Home',
+    'About',
+    'Contact',
+    'Service',
+    'Pricing',
+    'Blog',
+    'Features',
+    'Careers',
+    'Why Dazzle HR',
+    'Get Social'
+]
+
+const routes = { DashboardRoutes, extraRoutes }
+
+export default routes
