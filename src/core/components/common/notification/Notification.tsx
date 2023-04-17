28a6041cdef7ec868514280505d06ea34308b27b
Qodestackr <winchygichu@gmail.com>
2023-04-17 14:23:13 +0300

    .make

--- src/core/components/common/notification/Notification.tsx
+++ src/core/components/common/notification/Notification.tsx
@@ -1,21 +1,18 @@
 import React, { ReactNode, useState } from "react";
 
 // Define the different types of notifications that can be displayed
-type NotificationType = "error" | "warning" | "success" | "info";
+type NotificationType = "error" | "warning" | "success" | "info" | "loading"
 
 // Define the props for the Notification component
 interface NotificationProps {
     type: NotificationType;
+    title: String;
     children: ReactNode;
     onClose?: () => void;
 }
 
 // Define the Notification component
-const Notification: React.FC<NotificationProps> = ({
-    type,
-    children,
-    onClose,
-}) => {
+const Notification: React.FC<NotificationProps> = ({type, title, children,onClose,}) => {
     // Use state to keep track of whether the notification has been closed
     const [closed, setClosed] = useState(false);
 
@@ -47,71 +44,75 @@ const Notification: React.FC<NotificationProps> = ({
         switch (type) {
             case "error":
                 return (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
+                    // <svg
+                    //     xmlns="http://www.w3.org/2000/svg"
+                    //     className="h-6 w-6"
+                    //     fill="none"
+                    //     viewBox="0 0 24 24"
+                    //     stroke="currentColor"
+                    // >
+                    //     <path
+                    //         strokeLinecap="round"
+                    //         strokeLinejoin="round"
+                    //         strokeWidth={2}
+                    //         d="M6 18L18 6M6 6l12 12"
+                    //     />
+                    // </svg>
+                    null 
                 );
             case "warning":
                 return (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M12 9v2m0 4h.01m-6.938 3.993C5.458 18.784 5 17.95 5 17V7c0-.95.458-1.784 1.073-2.327C6.687 4.14 7.458 4 8.227 4h7.546c.769 0 1.54.14 2.154.673.615.543 1.073 1.377 1.073 2.327v10c0 .95-.458 1.784-1.073 2.327-.614.533-1.385.673-2.154.673H8.227c-.77 0-1.54-.14-2.154-.673z"
-                        />
-                    </svg>
+                    // <svg
+                    //     xmlns="http://www.w3.org/2000/svg"
+                    //     className="h-6 w-6"
+                    //     fill="none"
+                    //     viewBox="0 0 24 24"
+                    //     stroke="currentColor"
+                    // >
+                    //     <path
+                    //         strokeLinecap="round"
+                    //         strokeLinejoin="round"
+                    //         strokeWidth={2}
+                    //         d="M12 9v2m0 4h.01m-6.938 3.993C5.458 18.784 5 17.95 5 17V7c0-.95.458-1.784 1.073-2.327C6.687 4.14 7.458 4 8.227 4h7.546c.769 0 1.54.14 2.154.673.615.543 1.073 1.377 1.073 2.327v10c0 .95-.458 1.784-1.073 2.327-.614.533-1.385.673-2.154.673H8.227c-.77 0-1.54-.14-2.154-.673z"
+                    //     />
+                    // </svg>
+                    null
                 );
             case "success":
                 return (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M5 13l4 4L19 7"
-                        />
-                    </svg>
+                    // <svg
+                    //     xmlns="http://www.w3.org/2000/svg"
+                    //     className="h-6 w-6"
+                    //     fill="none"
+                    //     viewBox="0 0 24 24"
+                    //     stroke="currentColor"
+                    // >
+                    //     <path
+                    //         strokeLinecap="round"
+                    //         strokeLinejoin="round"
+                    //         strokeWidth={2}
+                    //         d="M5 13l4 4L19 7"
+                    //     />
+                    // </svg>
+                    null
                 );
             case "info":
                 return (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M12 6v2m0 4h.01M12 18v-6"
-                        />
-                    </svg>
+                    // <svg
+                    //     xmlns="http://www.w3.org/2000/svg"
+                    //     className="h-6 w-6"
+                    //     fill="none"
+                    //     viewBox="0 0 24 24"
+                    //     stroke="currentColor"
+                    // >
+                    //     <path
+                    //         strokeLinecap="round"
+                    //         strokeLinejoin="round"
+                    //         strokeWidth={2}
+                    //         d="M12 6v2m0 4h.01M12 18v-6"
+                    //     />
+                    // </svg>
+                    null
                 );
             default:
                 return null;
