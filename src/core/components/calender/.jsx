37eafa8e73f103fc021049052b5598abeb2fc9d4
Qodestackr <winchygichu@gmail.com>
2023-04-19 14:01:36 +0300

    rm::calendar/legacy

--- src/core/components/calender/.jsx
+++ src/core/components/calender/.jsx
@@ -79,7 +79,8 @@
 /**
  * 
  * 
- * import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+ * 
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
